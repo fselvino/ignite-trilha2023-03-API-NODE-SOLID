a5af6590965af47a702afb0904ddaa26e367f92f
fselvino <ferandoselvino@gmail.com>
2023-12-12 19:10:28 -0300

    Integraçao com Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {  
+    name:'Diego Fernando',
+    email: 'diego@rocketseat.com.br'    
+  }
+})
