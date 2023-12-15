5f4b0ce4c26c7b6d6739f37001e59d5410daab20
fselvino <ferandoselvino@gmail.com>
2023-12-14 22:32:52 -0300

    Criação de Usuário

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,26 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
 
 export const app = fastify()
-const prisma = new PrismaClient()
 
-prisma.user.create({
-  data: {  
-    name:'Diego Fernando',
-    email: 'diego@rocketseat.com.br'    
-  }
+import { prisma } from './lib/prisma'
+
+app.post('/users', async (request, reply)=> {
+
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email:z.string().email(),
+    password: z.string().min(6)
+  })
+
+  const {name, email, password} = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password
+    }
+  })
+  return reply.status(201).send()
 })
