9e08a36fb10956d6a7a7c5e6ecbff5d1a00613d3
fselvino <ferandoselvino@gmail.com>
2023-12-16 10:20:47 -0300

    Repository Pattern

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,6 @@
 import fastify from 'fastify'
-import { z } from 'zod'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
-import { prisma } from './lib/prisma'
-
-app.post('/users', async (request, reply)=> {
-
-  const registerBodySchema = z.object({
-    name: z.string(),
-    email:z.string().email(),
-    password: z.string().min(6)
-  })
-
-  const {name, email, password} = registerBodySchema.parse(request.body)
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password
-    }
-  })
-  return reply.status(201).send()
-})
+app.register(appRoutes)
